8f3c548d49efd383ba6c22eaeb86d749716c671f
Maria P <v1inthemaking@gmail.com>
2020-05-14 20:45:09 +0800

    viewsdone

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks',    to: 'tasks#all',    as: :tasks
+  get 'details',    to: 'tasks#details'
+  post 'add',    to: 'tasks#add'
+  post 'remove',    to: 'tasks#remove'
+  post 'edit',    to: 'tasks#edit'
 end
---
+++ db/migrate/20200514122340_add_nre_detaultto_tasks.rb
@@ -0,0 +1,4 @@
+class AddNreDetaulttoTasks < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
